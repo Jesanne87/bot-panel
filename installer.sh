d0a4dc0f4cb1739609c2184133b6c7d37a17c061
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-04-29 12:24:15 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 ipes=$(curl -sS ipv4.icanhazip.com)
-surat=$(curl -sS https://raw.githubusercontent.com/msi8888/allow/main/ipmini | grep -w $ipes | awk '{print $4}')
+surat=$(curl -sS https://raw.githubusercontent.com/msi8888/allow/main/ipmini | grep "$ipes" )
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 if [[ "$surat" = "true" ]]; then
   echo -ne
